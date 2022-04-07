b2da4af48346053e18e1cd7ed1783423323efadb
Silvana Zalazar <90283693+SilvanaZ@users.noreply.github.com>
2022-04-07 15:02:49 -0300

    Update app.js

--- myapp/app.js
+++ myapp/app.js
@@ -3,11 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var app = express();
 
+
+//Routes
 var indexRouter = require('./routes/indexRouter');
 var usersRouter = require('./routes/usersRouter');
-
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
